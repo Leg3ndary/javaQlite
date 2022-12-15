f1053a428d0d2fb3e57480754f36636b94ce5327
_Leg3ndary <ben.zhou.2006@gmail.com>
2022-12-14 23:04:26 -0500

    Push starting instructions

--- src/javaQlite.java
+++ src/javaQlite.java
@@ -1,7 +1,43 @@
+import java.util.Scanner;
+
 public class javaQlite {
+
+    public static String parseInput(Scanner scanner) {
+        String input = scanner.nextLine();
+        if (input.trim().equals("{exit()}")) {
+            System.exit(0);
+        }
+        return input;
+    }
+
+    
     
     public static void main(String[] args) {
-        Connect connect = new Connect();
-        connect.connect();
+        
+        boolean running = false;
+
+        Scanner scanner = new Scanner(System.in);
+        
+        while (running) {
+            System.out.println("Welcome to the JavaQlite program. Please select an option from the menu below:");
+            System.out.println("1. Create a new database");
+            System.out.println("2. Create a new table");
+            System.out.println("3. Insert data into a table");
+            System.out.println("4. Select data from a table");
+            System.out.println("5. Update data in a table");
+            System.out.println("6. Delete data from a table");
+            System.out.println("7. Delete a table");
+            System.out.println("8. Delete a database");
+            System.out.println("9: Run a custom query");
+            System.out.println("0. Exit (Write {exit()} to exit the program at any time)");
+            System.out.println("Please enter your selection: ");
+
+            String input = parseInput(scanner);
+            System.out.println("You entered: " + input);
+
+
+            Connect connect = new Connect();
+            connect.connect();
+        }
     }
 }
